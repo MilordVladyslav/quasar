310eb717195efeea83e3c9ff434f090ce57300ce
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2018-11-19 15:17:35 +0200

    v.1.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,17 @@
-(function ($) {
-    'use srtict'
+window.onload = function() {
+    'use strict'
+
     
-})(jQuery)
+    
+    let bit = 500;
+    document.querySelector('#add').addEventListener('click', function(e){
+        bit += 100;
+        c(bit);
+        document.querySelector('#bit').value = bit;
+    })
+    document.querySelector('#bit').value = bit;
+
+    function c (arg) {
+        console.log(arg)
+    }
+};
