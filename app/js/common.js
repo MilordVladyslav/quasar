f9b11481b96934c0181374e020ee887be5666310
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2018-11-20 14:53:43 +0200

    v.1.1

--- app/js/common.js
+++ app/js/common.js
@@ -2,14 +2,32 @@ window.onload = function() {
     'use strict'
 
     
-    
+    let totalCredits = 1234;
+
+    document.querySelector('#total-credits').innerHTML = `$${totalCredits}`;
+
     let bit = 500;
+
     document.querySelector('#add').addEventListener('click', function(e){
-        bit += 100;
-        c(bit);
+        bit < totalCredits - 100 ? bit += 100 : bit += totalCredits - bit;
         document.querySelector('#bit').value = bit;
     })
+    
+    document.querySelector("#substract").addEventListener('click', function(e) {
+        if(bit > 0) bit -= 100;
+        if(bit < 0) bit = 0;
+        document.querySelector('#bit').value = bit;
+    })
+
+    function Random_1_8 () {
+        let rand = Math.floor(Math.random() * 8)
+        c(rand);
+    }   
+
+    
     document.querySelector('#bit').value = bit;
+
+    
 
     function c (arg) {
         console.log(arg)
