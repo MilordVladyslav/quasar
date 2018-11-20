bc4564a4e866215b822d1b4d245ff5a8be881cb2
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2018-11-20 16:19:31 +0200

    v.1.2

--- app/js/common.js
+++ app/js/common.js
@@ -16,18 +16,48 @@ window.onload = function() {
     document.querySelector("#substract").addEventListener('click', function(e) {
         if(bit > 0) bit -= 100;
         if(bit < 0) bit = 0;
+
+        
         document.querySelector('#bit').value = bit;
     })
 
     function Random_1_8 () {
-        let rand = Math.floor(Math.random() * 8)
-        c(rand);
+        let rand = Math.floor(Math.random() * 8 + 1)
+        c(rand)
+        return rand
     }   
 
+    function Random_4_7 () {
+        let rand = Math.floor(Math.random() * (7 - 4 + 1)) + 4
+        c(rand)
+        return rand
+    }
+
+    let rand = document.querySelectorAll(".rand");
+    document.querySelector("#random-1-8").addEventListener('click', function() {
+        for(let i = 0; i<rand.length; i++) {
+            rand[i].classList.remove('active')
+        }
+        rand[0].classList.add('active')
+
+        
+    })
+
+    document.querySelector("#random-4-7").addEventListener('click', function() {
+        
+        for(let i = 0; i<rand.length; i++) {
+            rand[i].classList.remove('active')
+        }
+        rand[1].classList.add('active')
+    })
     
     document.querySelector('#bit').value = bit;
 
-    
+    document.querySelector('#play').addEventListener('click', function() {
+        if(rand[0].classList.contains('active')) {
+            Random_1_8();
+        }
+    })
 
     function c (arg) {
         console.log(arg)
