2c3261a8ebb794191d4309a48f8f72772fafdb96
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2018-11-21 14:06:26 +0200

    v.1.3

--- app/js/common.js
+++ app/js/common.js
@@ -2,11 +2,12 @@ window.onload = function() {
     'use strict'
 
     
-    let totalCredits = 1234;
+    let totalCredits = 5000;
 
     document.querySelector('#total-credits').innerHTML = `$${totalCredits}`;
 
     let bit = 500;
+    let errors = [];
 
     document.querySelector('#add').addEventListener('click', function(e){
         bit < totalCredits - 100 ? bit += 100 : bit += totalCredits - bit;
@@ -22,15 +23,13 @@ window.onload = function() {
     })
 
     function Random_1_8 () {
-        let rand = Math.floor(Math.random() * 8 + 1)
-        c(rand)
-        return rand
+        let randomDigit = Math.floor(Math.random() * 8 + 1)
+        return randomDigit
     }   
 
     function Random_4_7 () {
-        let rand = Math.floor(Math.random() * (7 - 4 + 1)) + 4
-        c(rand)
-        return rand
+        let randomDigit = Math.floor(Math.random() * (7 - 4 + 1)) + 4
+        return randomDigit
     }
 
     let rand = document.querySelectorAll(".rand");
@@ -53,11 +52,64 @@ window.onload = function() {
     
     document.querySelector('#bit').value = bit;
 
+    let userResult = 0;
+
+    document.querySelector('#user-result').innerHTML = userResult;
+
+    let userValue;
+
     document.querySelector('#play').addEventListener('click', function() {
+        let randomDigit;
         if(rand[0].classList.contains('active')) {
-            Random_1_8();
+            randomDigit = Random_1_8();
         }
+        if(rand[1].classList.contains('active')) {
+            randomDigit = Random_4_7();
+        } 
+
+        userResult += randomDigit
+        
+        userValue = document.querySelector('#user-result').innerHTML = userResult;
+        
+        if(userValue > 20) {
+            prize(userValue);
+        }
+    
     })
+
+    document.querySelector('#stop').addEventListener('click', function() {
+        prize(userValue);
+    })
+    
+
+    function prize (userValue) {
+        if(userValue > 16 ) {
+            switch(userValue) {
+                case 17:
+                return PercentToMoney(25, bit)
+                case 18: 
+                return PercentToMoney(50, bit)
+                case 19:
+                return PercentToMoney(100, bit)
+                case 20: 
+                return PercentToMoney(200, bit)
+            }
+        } else {
+            return 'You defeat'
+        }
+    }
+
+    
+
+    function PercentToMoney(percent, bit) {
+        let result = percent / 100 * bit;
+        bit += result;
+        totalCredits += bit;
+        return `You Win ${bit += result} Credits (${percent}$)`
+    }
+
+
+
 
     function c (arg) {
         console.log(arg)
